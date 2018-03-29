98078b9da15eb14c12aa5852872bfbe1e2529acc
Tito Yslas <roberto.yslas@gmail.com>
2018-03-29 16:43:27 -0600

    learning about arraylists

---
+++ Sec9-arrays-lists-autoboxing-unboxing/2-list-arraylist-pt1/src/com/tito/GroceryList.java
@@ -0,0 +1,28 @@
+package com.tito;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+  private ArrayList<String> groceryList = new ArrayList<String>();
+
+  public void addGroceryItem(String item) {
+    groceryList.add(item);
+  }
+
+  public void printGroceryList() {
+    System.out.println("you have " + groceryList.size() + " items in your grocery list");
+    for (int i = 0; i < groceryList.size(); i++) {
+      System.out.println((i + 1) + ". " + groceryList.get(i));
+    }
+  }
+
+  public void modifyGroceryItem(int position, String newItem) {
+    groceryList.set(position, newItem);
+    System.out.println("grocery item " + (position + 1) + " has been modified");
+  }
+
+  public  void removeGroceryItem(int position) {
+    String theItem = groceryList.get(position);
+    groceryList.remove(position);
+  }
+}
