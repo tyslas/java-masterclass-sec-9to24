739a9e8873ce4993b90411f5b623c0b514b9f689
Tito Yslas <roberto.yslas@gmail.com>
2018-04-11 14:59:44 -0600

    part 1 - learning about LinkedLists

--- Sec9-arrays-lists-autoboxing-unboxing/4-linked-list/src/com/tito/Main.java
+++ Sec9-arrays-lists-autoboxing-unboxing/4-linked-list/src/com/tito/Main.java
@@ -1,8 +1,29 @@
 package com.tito;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    Customer customer = new Customer("cisco", 24.36);
+    Customer anotherCustomer;
+    anotherCustomer = customer;
+    anotherCustomer.setBalance(82.49);
+    System.out.println("balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+    ArrayList<Integer> intList = new ArrayList<Integer>();
+    intList.add(1);
+    intList.add(3);
+    intList.add(4);
+
+    for (int i = 0; i < intList.size(); i++) {
+      System.out.println(i + ": " + intList.get(i));
     }
+
+    intList.add(1, 2);
+
+    for (int i = 0; i < intList.size(); i++) {
+      System.out.println(i + ": " + intList.get(i));
+    }
+  }
 }
